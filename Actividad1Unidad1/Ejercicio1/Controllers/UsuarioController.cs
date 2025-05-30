44d4ac140fe5af289b57fa2a5bd3171206a00abe
Luis Fortuna <javier-uribe@hotmail.com>
2025-05-29 21:16:13 -0400

    Agregando controlador para usuario.

---
+++ Actividad1Unidad1/Ejercicio1/Controllers/UsuarioController.cs
@@ -0,0 +1,31 @@
+﻿using Ejercicio1.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Ejercicio1.Controllers
+{
+    public class UsuarioController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        public IActionResult Registrar()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Registrar(UsuarioViewModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                ViewBag.Message = "El usuario ha sido registrado.";
+                return View(model);
+            }
+
+            return View(model);
+        }
+    }
+}
---
+++ Actividad1Unidad1/Ejercicio1/Models/UsuarioViewModel.cs
@@ -0,0 +1,21 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Ejercicio1.Models
+{
+    public class UsuarioViewModel
+    {
+        [Required(ErrorMessage ="El nombre de usuario es obligatorio completarlo.")]
+        [StringLength(20, MinimumLength = 3, ErrorMessage = "El nombre de usuario debe de estar entre 3 y 20 caracteres.")]
+        [Display(Name ="Usuario")]
+        public string NombreUsuario { get; set; }
+
+        [Required(ErrorMessage ="El correo electrónico es obligatorio.")]
+        [EmailAddress(ErrorMessage = "El correo electrónico no es válido")]
+        [Display(Name ="Correo Electrónico")]
+        public string CorreoElectronico { get; set; }
+        [Display(Name ="Contraseña")]
+        public string Contrasena { get; set; }
+        [Display(Name ="Confirmar Contraseña")]
+        public string ConfirmarContrasena { get; set; }
+    }
+}
